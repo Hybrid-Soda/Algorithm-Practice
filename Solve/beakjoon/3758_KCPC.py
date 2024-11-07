21988fb9cd83116e229a01932b10f5fc1ff37f9e
임경태 <limkt0925@gmail.com>
2024-11-07 23:04:10 +0900

    3758_KCPC

---
+++ Solve/beakjoon/3758_KCPC.py
@@ -0,0 +1,21 @@
+# 3758 KCPC / 구현, 정렬
+
+import sys
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    n, k, t, m = map(int, input().split())
+
+    score = {i: [0] * (k+1) for i in range(1, n+1)}
+    count = [0] * (n+1)
+    order = [0] * (n+1)
+
+    for l in range(m):
+        i, j, s = map(int, input().split())
+
+        score[i][j] = max(score[i][j], s)
+        count[i] += 1
+        order[i] = l
+    
+    rank = sorted(score, key=lambda x: (-sum(score[x]), count[x], order[x]))
+    print(rank.index(t)+1)
