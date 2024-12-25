216c6ec1e6d127570d0953b388548c2339b3f27a
임경태 <limkt0925@gmail.com>
2024-12-26 00:04:23 +0900

    3079_입국심사

--- Solve/beakjoon/3079_입국심사.py
+++ Solve/beakjoon/3079_입국심사.py
@@ -1,35 +1,26 @@
-# 3079 입국심사
-
-import sys
-input = sys.stdin.readline
-
-
-def confirm(m: int, times: list[int], cutline: int):
-    for time in times:
-        if time <= cutline:
-            m -= cutline // time
-        else:
-            break
-    print('m:', m)
-    return m
-
+# 3079 입국심사 / 이분 탐색, 매개 변수 탐색
 
 def binary_search(m: int, times: list[int]):
-    min_time, max_time = 1, m * max(times)
+    min_time, max_time = times[0], m * times[-1]
+    answer = float('inf')
 
     while min_time <= max_time:
         mid_time = (min_time + max_time) // 2
-        print(mid_time)
-        res = confirm(m, times, mid_time)
+        cnt_temp = 0
+        
+        # 중간 시간 동안 처리할 수 있는 인원 수 계산
+        for time in times:
+            cnt_temp += mid_time // time
 
-        if res > 0:
+        # 처리할 수 있는 인원 수가 m 이상이면 시간을 줄인다
+        if cnt_temp >= m:
             max_time = mid_time - 1
-        elif res < 0:
-            min_time = mid_time + 1
+            answer = min(answer, mid_time)
+        # 처리할 수 있는 인원 수가 m 미만이면 시간을 늘린다
         else:
-            return mid_time
+            min_time = mid_time + 1
 
-    return max_time
+    return answer
 
 n, m = map(int, input().split())
 times = sorted(int(input()) for _ in range(n))
