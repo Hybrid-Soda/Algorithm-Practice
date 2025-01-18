d9e6418a2c16d7688135bfbdcba65ff5c3e95ba3
임경태 <limkt0925@gmail.com>
2025-01-18 23:20:59 +0900

    2293_동전1

---
+++ Solve/beakjoon/2293_동전1.py
@@ -0,0 +1,13 @@
+# 2293 동전 1
+
+n, k = map(int, input().split())
+coins = [int(input()) for _ in range(n)]
+dp = [0] * (k+1)
+dp[0] = 1
+
+for coin in coins:
+    for i in range(coin, k+1):
+        # 현재 금액을 만드는 방법의 수에 이전 금액을 만드는 방법의 수를 더함
+        dp[i] += dp[i-coin]
+
+print(dp[-1])
