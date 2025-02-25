8b182c2189f28a6d27605915952d61be3e876080
임경태 <limkt0925@gmail.com>
2025-02-25 22:41:03 +0900

    2476_주사위_게임

---
+++ Solve/beakjoon/2476_주사위_게임.py
@@ -0,0 +1,20 @@
+# 2476 주사위 게임 / 수학, 구현, 사칙연산
+
+n = int(input())
+max_prize = 0
+
+for _ in range(n):
+    a, b, c = map(int, input().split())
+    
+    if a == b == c:
+        prize = 10000 + a * 1000
+    elif a == b or a == c:
+        prize = 1000 + a * 100
+    elif b == c:
+        prize = 1000 + b * 100
+    else:
+        prize = max(a, b, c) * 100
+    
+    max_prize = max(max_prize, prize)
+
+print(max_prize)
