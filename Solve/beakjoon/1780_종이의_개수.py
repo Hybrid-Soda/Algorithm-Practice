8b365f85fcbb1040b0f42cf7347db4c759b502d1
임경태 <limkt0925@gmail.com>
2025-04-05 00:14:24 +0900

    1780_종이의_개수

--- Solve/beakjoon/1780_종이의_개수.py
+++ Solve/beakjoon/1780_종이의_개수.py
@@ -1,23 +1,25 @@
 # 1780 종이의 개수
 
-def check_same(paper, count):
+def check_same(paper, count, n):
+    m = n//3
     paper_set = set()
 
     for row in paper:
         paper_set.update(row)
-    # print(paper)
-    # print(paper_set)
-    # print()
+    
+    num_cnt = len(paper_set)
 
-    if len(paper_set) > 1:
-        for i in range(0, n+1, n//3):
-            for j in range(0, n+1, n//3):
-                check_same(paper[i:i+(n//3)][j:j+(n//3)], count)
-    else:
-        count[paper[0][0] + 1] += 1
+    if num_cnt > 1:
+        for i in range(0, n, m):
+            for j in range(0, n, m):
+                check_same([paper[k][j:j+m] for k in range(i, i+m)], count, m)
+    elif num_cnt == 1:
+        count[paper_set.pop() + 1] += 1
 
 n = int(input())
 paper = [list(map(int, input().split())) for _ in range(n)]
 count = [0] * 3
-print(paper[0:3][0:3])
-check_same(paper, count)
+check_same(paper, count, n)
+
+for i in count:
+    print(i)
