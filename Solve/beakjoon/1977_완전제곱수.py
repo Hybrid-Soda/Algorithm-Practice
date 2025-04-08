4a763dd3d0a18bbea7358b6c71105d535a53ba34
임경태 <limkt0925@gmail.com>
2025-04-08 22:54:35 +0900

    1977_완전제곱수

--- Solve/beakjoon/1977_완전제곱수.py
+++ Solve/beakjoon/1977_완전제곱수.py
@@ -1,12 +1,12 @@
 # 1977 완전제곱수 / 수학, 구현, 브루트포스 알고리즘
 
-M = int(input())
-N = int(input())
+m = int(input())
+n = int(input())
 sum = 0
 min = 0
 
 for i in range(1, 101):
-    if M <= i**2 and N >= i**2:
+    if m <= i**2 and n >= i**2:
         if sum == 0:
             min = i**2
         sum += i**2
