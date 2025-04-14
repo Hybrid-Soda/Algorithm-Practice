4b74bb3fde3a2254ed027b68eccc701cc016dca9
임경태 <limkt0925@gmail.com>
2025-04-14 22:34:59 +0900

    1431_시리얼_번호

---
+++ Solve/beakjoon/1431_시리얼_번호.py
@@ -0,0 +1,19 @@
+# 1431 시리얼 번호 / 문자열, 정렬
+
+n = int(input())
+
+def sum_num(inputs):
+    result = 0
+    for i in inputs:
+        if i.isdigit():
+            result+=int(i)
+    return result
+
+arr = []
+for i in range(n):
+    a = input()
+    arr.append(a)
+
+arr.sort(key=lambda x: (len(x), sum_num(x), x))
+for i in arr:
+    print(i)
