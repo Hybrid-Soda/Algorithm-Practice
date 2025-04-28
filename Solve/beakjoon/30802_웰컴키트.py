7cde1558d21ed0bab823e86b83e092c662360b69
임경태 <limkt0925@gmail.com>
2025-04-28 23:34:42 +0900

    30802_웰컴키트

---
+++ Solve/beakjoon/30802_웰컴키트.py
@@ -0,0 +1,17 @@
+# 30802 웰컴 키트 / 수학, 구현, 사칙연산
+
+n = int(input())
+size = list(map(int, input().split()))
+t, p = map(int, input().split())
+score = 0
+sum_score = 0
+
+for i in range(len(size)):
+    if (size[i]%t) == 0:
+        score = size[i]//t
+    else:
+        score = size[i]//t + 1
+    sum_score += score
+
+print(sum_score)
+print(n//p, n%p)
