3da1d53e5ec4b128e2067800360dd857c5f476a6
임경태 <limkt0925@gmail.com>
2025-05-10 00:37:24 +0900

    2108_통계학

--- Solve/beakjoon/2108_통계학.py
+++ Solve/beakjoon/2108_통계학.py
@@ -1,8 +1,8 @@
-# 2108 통계학
+# 2108 통계학 / 수학, 구현, 정렬, 집합과 맵
 
-def get_mode(nums):
+def get_mode(n, nums):
     modes = [nums[0]]
-    mode_count = 1
+    mode_count = 0
     temp = 1
 
     for i in range(1, n):
@@ -10,18 +10,21 @@ def get_mode(nums):
             temp += 1
         else:
             if temp > mode_count:
-                modes = [nums]
+                modes = [nums[i-1]]
+                mode_count = temp
             elif temp == mode_count:
                 modes.append(nums[i-1])
-            else:
-                temp = 1
-    
+            temp = 1
+    if temp > mode_count:
+        modes = [nums[-1]]
+    elif temp == mode_count:
+        modes.append(nums[-1])
     return modes[0] if len(modes) < 2 else modes[1]
 
 n = int(input())
 nums = sorted(int(input()) for _ in range(n))
 
-print(sum(nums)//n)
+print(round(sum(nums)/n))
 print(nums[n//2])
-print(get_mode)
+print(get_mode(n, nums))
 print(nums[-1] - nums[0])
