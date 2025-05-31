71d372570137aa67c9323920b173ea4532806209
임경태 <limkt0925@gmail.com>
2025-05-31 23:42:26 +0900

    1932 정수 삼각형

--- Solve/beakjoon/1932_정수_삼각형.py
+++ Solve/beakjoon/1932_정수_삼각형.py
@@ -2,7 +2,6 @@
 
 n = int(input())
 dp = [list(map(int, input().split())) for _ in range(n)]
-
 for i in range(1, n):
     for j in range(i+1):
         if j == 0:
@@ -11,5 +10,4 @@ for i in range(1, n):
             dp[i][j] += dp[i-1][j-1]
         else:
             dp[i][j] += max(dp[i-1][j], dp[i-1][j-1])
-
 print(max(dp[n-1]))
