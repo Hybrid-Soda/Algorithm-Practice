8c692c0e9f7260cbedbfd0889737a3f4b8e72ff5
임경태 <limkt0925@gmail.com>
2025-07-08 22:02:02 +0900

    12939 최댓값과 최솟값

---
+++ Solve/programmers/12939_최댓값과 최솟값.java
@@ -0,0 +1,19 @@
+// 12939 최댓값과 최솟값
+
+class Solution {
+    public String solution(String s) {
+        String[] sList = s.split(" ");
+
+        int maxV = Integer.MIN_VALUE;
+        int minV = Integer.MAX_VALUE;
+
+        for (String num : sList) {
+            int n = Integer.parseInt(num);
+            maxV = Math.max(maxV, n);
+            minV = Math.min(minV, n);
+        }
+
+        String answer = minV + " " + maxV;
+        return answer;
+    }
+}
