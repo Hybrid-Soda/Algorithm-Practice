64de1e59881da9337aad7a93cb776ee9a7b22409
임경태 <limkt0925@gmail.com>
2025-07-12 23:33:21 +0900

    12937 짝수와 홀수

---
+++ Solve/programmers/12937_짝수와_홀수.java
@@ -0,0 +1,11 @@
+// 12937 짝수와 홀수
+
+class Solution {
+    String solution(int num) {
+        if (num % 2 == 0) {
+            return "Even";
+        } else {
+            return "Odd";
+        }
+    }
+}
