f26214f2f22140473ba84fbd6cd4632b0e334487
임경태 <limkt0925@gmail.com>
2025-07-22 00:21:54 +0900

    76501 음양 더하기

---
+++ Solve/programmers/76501_음양_더하기.java
@@ -0,0 +1,13 @@
+// 76501 음양 더하기
+
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+
+        for (int i = 0; i < signs.length; i++) {
+            answer += (signs[i]) ? absolutes[i] : -absolutes[i];
+        }
+
+        return answer;
+    }
+}
