a979d20d1a6c012d05acd846215af9a724073e95
임경태 <limkt0925@gmail.com>
2025-07-27 23:29:52 +0900

    12943 콜라츠 추측

---
+++ Solve/programmers/12943_콜라츠_추측.java
@@ -0,0 +1,15 @@
+// 12943 콜라츠 추측
+
+class Solution {
+    public int solution(long num) {
+        
+        for (int i = 0; i < 500; i++) {
+            
+            if (num == 1) return i; 
+            
+            num = (num % 2 == 0) ? num/2 : num*3+1;
+        }
+
+        return -1;
+    }
+}
