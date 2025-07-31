5d391b7e27d1d04d45d00ccd6e0eea98f2d9e348
임경태 <limkt0925@gmail.com>
2025-07-31 10:30:00 +0900

    70128 내적

---
+++ Solve/programmers/70128_내적.java
@@ -0,0 +1,22 @@
+// 70128 내적
+
+class Solution {
+    public int solution(int[] a, int[] b) {
+        int dot = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            dot += a[i] * b[i];
+        }
+
+        return dot;
+    }
+}
+
+
+// import java.util.stream.IntStream;
+
+// class Solution {
+//     public int solution(int[] a, int[] b) {
+//         return IntStream.range(0, a.length).map(idx -> a[idx] * b[idx]).sum();
+//     }
+// }
