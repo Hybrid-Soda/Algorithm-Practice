0d88a1980605d86618dd8ed6aa5c880fad237aeb
임경태 <limkt0925@gmail.com>
2025-08-05 23:54:12 +0900

    12918 문자열 다루기 기본

---
+++ Solve/programmers/12918_문자열 다루기 기본.java
@@ -0,0 +1,25 @@
+// 12918 문자열 다루기 기본
+
+
+class Solution {
+    public boolean solution(String s) {
+        if (s.length() == 4 || s.length() == 6) {
+            return s.matches("(^[0-9]*$)");
+        }
+        return false;
+    }
+}
+// class Solution {
+//     public boolean solution(String s) {
+//         int sLen = s.length();
+        
+//         if (sLen != 4 && sLen != 6) return false;
+
+//         try {
+//             int x = Integer.parseInt(s);
+//             return true;
+//         } catch (NumberFormatException e) {
+//             return false;
+//         }
+//     }
+// }
