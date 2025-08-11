a9f46473c1c55baac4079ac7df16ae776242d4c9
임경태 <limkt0925@gmail.com>
2025-08-11 23:12:00 +0900

    68935 3진법 뒤집기

---
+++ Solve/programmers/68935_3진법 뒤집기.java
@@ -0,0 +1,31 @@
+// 68935 3진법 뒤집기
+
+import java.util.ArrayList;
+
+class Solution {
+    public int solution(int n) {
+        return getDecimal(getReversedTernary(n));
+    }
+
+    private ArrayList getReversedTernary(int n) {
+        ArrayList ternary = new ArrayList();
+        
+        while (n > 0) {
+            ternary.add(n % 3);
+            n /= 3;
+        }
+
+        return ternary;
+    }
+
+    private int getDecimal(ArrayList ternery) {
+        int answer = 0;
+        int exponent = ternery.size() - 1;
+
+        for (int i = 0; i <= exponent; i++) {
+            answer += (int) ternery.get(i) * Math.pow(3, exponent - i);
+        }
+
+        return answer;
+    }
+}
