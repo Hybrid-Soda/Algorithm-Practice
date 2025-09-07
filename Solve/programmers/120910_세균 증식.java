f96e14a4a399f4eb785055131258c44e2e282294
임경태 <limkt0925@gmail.com>
2025-09-07 23:34:00 +0900

    120910_세균 증식

---
+++ Solve/programmers/120910_세균 증식.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n, int t) {
+        return (int) (n*Math.pow(2, t));
+    }
+}
+
+// class Solution {
+//     public int solution(int n, int t) {
+//         return n << t;
+//     }
+// }
