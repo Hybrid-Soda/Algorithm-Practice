15d5cc5b74b811218b8cad283ac4576233061f74
임경태 <limkt0925@gmail.com>
2026-01-09 02:19:16 +0900

    selection sort

--- Summary-Java/02_algorithm/Main.java
+++ Summary-Java/02_algorithm/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int[] arr = new int[] {9, 5, 71, 12};
-        Sort.bubbleSort(arr);
+        Sort.bubbleSort(new int[]{9, 5, 71, 12}, 4);
+        Sort.selectionSort(new int[]{9, 5, 71, 12}, 4);
     }
 }
--- Summary-Java/02_algorithm/Sort.java
+++ Summary-Java/02_algorithm/Sort.java
@@ -1,19 +1,35 @@
 import java.util.Arrays;
 
 public class Sort {
-    public static void bubbleSort(int[] arr) {
-        int n = arr.length;
+    private static void change(int[] arr, int i, int j) {
+        int temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temp;
+    }
 
+    // 인접 원소 교환 반복 방식
+    public static void bubbleSort(int[] arr, int n) {
         for (int i = 0; i < n; i++) {
             for (int j = n - 1; j > i; j--) {
                 if (arr[j] < arr[j - 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j - 1];
-                    arr[j - 1] = temp;
+                    change(arr, j, j - 1);
                 }
             }
         }
+        System.out.println(Arrays.toString(arr));
+    }
 
+    // 최솟값 선택 후 교환 방식
+    public static void selectionSort(int[] arr, int n) {
+        for (int i = 0; i < n - 1; i++) {
+            int minIdx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[minIdx] > arr[j]) {
+                    minIdx = j;
+                }
+            }
+            change(arr, i, minIdx);
+        }
         System.out.println(Arrays.toString(arr));
     }
 }
